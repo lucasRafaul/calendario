83127802d588d9c31abb89c0daadf42a3d74f5df
lucas <rafaullucas@gmail.com>
2024-06-05 03:31:19 -0300

    horario y docente form

--- src/components/CalendarDate.jsx
+++ src/components/CalendarDate.jsx
@@ -3,12 +3,18 @@ import { Dialog, DialogTitle, DialogContent } from '@mui/material';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { es } from 'date-fns/locale';
+import Horario from './Horario';
 
 const CalendarDate = ({ open, onClose, onDateClick }) => {
+
+  const [horarioOpen, setHorarioOpen] = useState(false);
+
   const handleDateClick = (date) => {
     onDateClick(date);
     onClose();
+    setHorarioOpen(true);
   };
+
   const tileDisabled = ({ date, view }) => {
     if (view === 'month') {
       return date < new Date().setHours(0, 0, 0, 0);
@@ -17,12 +23,15 @@ const CalendarDate = ({ open, onClose, onDateClick }) => {
   };
 
   return (
+    <div>
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Seleccione una Fecha</DialogTitle>
       <DialogContent>
         <Calendar onClickDay={handleDateClick} locale='es'  tileDisabled={tileDisabled}/>
       </DialogContent>
     </Dialog>
+   { horarioOpen && <Horario/>} 
+    </div>
   );
 };
 
